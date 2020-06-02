9483bafd49c7181d276b211700a2e91a8b55cd51
tylergreason <tyler.greason@protonmail.com>
2020-06-02 10:37:13 -0400

    Fixed iPhone issue by targeting it with a class through userAgent

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const newProjectCard = projectCard()
-// document.appendChild(newProjectCard); 
+console.log(navigator.userAgent)
+
+// fix cover image not showing right on iPhone
+// find background element 
+const cover = document.getElementById('cover'); 
+// if 'iPhone' is in the userAgent, add ios class to the cover element 
+if (navigator.userAgent.includes('iPhone')){
+    cover.classList.add('iPhone')
+}
