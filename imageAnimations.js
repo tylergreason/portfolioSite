2d478eed7d06933c2435516936c0873350418559
tylergreason <tyler.greason@protonmail.com>
2020-07-24 11:55:17 -0400

    Images are rotating classes almost correctly. Still needs lots of refinement.

--- imageAnimations.js
+++ imageAnimations.js
@@ -1,28 +1,47 @@
 const stdmImgs = Array.from(document.getElementsByClassName('stdm__img')); 
 
-let startImgIndex = 0; 
+let startImgIndex = 0;
+
+const increaseIndex = () => {
+    if (startImgIndex >= stdmImgs.length -1){
+        startImgIndex = 0;  
+    }else{
+        startImgIndex +=1; 
+    }
+}
+
+const toggleClass = (array, index, classToToggle) => {
+    array[index].classList.toggle(classToToggle)
+}
 
 // remove each open class from the imgs 
 const removeAllClasses = (array, classToRemove) => {
     array.forEach(img => img.classList.remove(classToRemove))
-
 }
 
 // add open class to next img 
-const animateNextImg = (index, array, classToAdd) => {
-    // console.log(index);
-    // console.log(startImgIndex);
-    if (startImgIndex >= array.length-1){
-        startImgIndex = 0; 
+const animateNextImg = (array, classToAdd) => {
+    array.forEach((img, i) =>{
+        if (img.classList.contains(classToAdd)){
+            img.classList.toggle(classToAdd); 
+
+            // check if this is the last img in the array
+            if (i === array.length-1){
+                // console.log(array[0]);
+                return array[0].classList.add(classToAdd); 
+            }else {
+                // console.log(array[i+1]);
+                return array[i+1].classList.add(classToAdd); 
+            }
+        }
+        
+        })
     }
-    array[index].classList.add(classToAdd); 
-    startImgIndex += 1; 
-    console.log(startImgIndex);
+
+const animateImgs = (array,classToToggle) => {
+    removeAllClasses(array, classToToggle)
+    toggleClass(array, startImgIndex, classToToggle); 
+    increaseIndex()
 }
 
-const animateImgs = (index,array,classToAdd, classToRemove) => {
-    removeAllClasses(array, classToRemove); 
-    index = animateNextImg(index, array, classToAdd); 
-}
-
-window.setInterval(() => animateImgs(startImgIndex, stdmImgs, "stdm__img__open", "stdm__img__open"), 1500)
+window.setInterval(() => animateImgs(stdmImgs, "stdm__img__open"), 1500)
